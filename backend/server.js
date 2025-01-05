2387bf0a1aabdb4ff2c52e3a6cfd353614d81441
Yash Gupta <yashgupta13.in@gmail.com>
2025-01-05 23:18:39 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -16,9 +16,9 @@ app.use(express.json());
 // Enable CORS for all routes
 app.use(
   cors({
-    origin: "*", // Allow all origins
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allow all methods
-    allowedHeaders: ["Content-Type", "Authorization", "*"], // Allow all headers
+    origin: ["http://localhost:3001", "*","https://udaan-kra.netlify.app"], 
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
